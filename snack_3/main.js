cb4e7d1d6f1e0d6751f2e0ee6bb741f8762b3e52
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-11 16:45:16 +0200

    add snack 3

--- snack_3/main.js
+++ snack_3/main.js
@@ -1,8 +1,14 @@
 /* Snack 3 (Bonus)
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. */
-
-function check (a,b,c) {
-    let number [10, 20]
-
+let risultato = []
+let a = 30;
+let b = 50;
+function posizioneCompresa (array,a,b) {
+    for (let i = 0; i < array.length; i++) {
+        if (a <= array[i] && array[i] <= b) {
+          risultato.push(array[i]);
+        }
+      }  
 }
+
